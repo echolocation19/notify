b7d6f7d4d364716037d10c44f88f66fe554f244e
echolocation19 <sakura2420@mail.ru>
2024-10-09 15:05:40 -0700

    fix navigation

--- app/src/main/java/com/nevrmd/notify/view/NoteFragment.kt
+++ app/src/main/java/com/nevrmd/notify/view/NoteFragment.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.nevrmd.notify.R
-import com.nevrmd.notify.databinding.FragmentHomeBinding
 import com.nevrmd.notify.databinding.FragmentNoteBinding
 import com.nevrmd.notify.model.Note
 import com.nevrmd.notify.persistence.NoteDatabase
@@ -27,18 +27,19 @@ class NoteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentNoteBinding.inflate(inflater, container, false)
+        val view = binding.root
         val dao = NoteDatabase.getInstance(inflater.context).noteDao
         val repository = NoteRepository(dao)
         val factory = MainViewModelFactory(repository)
         mainViewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
-
         binding.fabSave.setOnClickListener {
             val title = binding.etTitle.text.toString()
             val body = binding.etBody.text.toString()
             val note: Note = Note(0, title, body)
             mainViewModel.insert(note)
+            Navigation.findNavController(view).navigate(R.id.navigateToHomeFragment)
         }
 
-        return binding.root
+        return view
     }
 }
